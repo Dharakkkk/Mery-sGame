c2a1b9de113460ffb3673bcc4cd90481edcaeff7
Dharakkkk <uscho@o2.pl>
2024-03-01 23:57:51 +0100

    Normal sizes

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const GAME_SPEED_START = 1; // 1.0
 const GAME_SPEED_INCREMENT = 0.00001;
 
 const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
-const PLAYER_WIDTH = 420 / 1.5; //58
-const PLAYER_HEIGHT = 560 / 1.5; //62
-const MAX_JUMP_HEIGHT = 600;
+const GAME_HEIGHT = 200;
+const PLAYER_WIDTH = 88 / 1.5; //58
+const PLAYER_HEIGHT = 94 / 1.5; //62
+const MAX_JUMP_HEIGHT = GAME_HEIGHT;
 const MIN_JUMP_HEIGHT = 150;
 const GROUND_WIDTH = 2400;
 const GROUND_HEIGHT = 24;
 const GROUND_AND_CACTUS_SPEED = 0.5;
 
 const CACTI_CONFIG = [
-  { width: 200 / 1.5, height: 200 / 1.5, image: "images/cactus_1.png" },
-  { width: 300 / 1.5, height: 300 / 1.5, image: "images/cactus_2.png" },
-  { width: 400 / 1.5, height: 400 / 1.5, image: "images/cactus_3.png" },
+  { width: 48 / 1.5, height: 100 / 1.5, image: "images/cactus_1.png" },
+  { width: 98 / 1.5, height: 100 / 1.5, image: "images/cactus_2.png" },
+  { width: 68 / 1.5, height: 70 / 1.5, image: "images/cactus_3.png" },
 ];
 
 //Game Objects
