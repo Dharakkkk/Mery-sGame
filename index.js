de224dc3271640ca4c85db116d60042e2386f57a
Dharakkkk <uscho@o2.pl>
2024-03-02 16:31:38 +0100

    Revert "Niestabilna wersja"
    This reverts commit 63a717c27084c0bdff7ca608981dc57429783700.

--- index.js
+++ index.js
@@ -1,72 +1,86 @@
-// Importy klas (zakładam, że masz je w osobnych plikach)
 import Player from "./Player.js";
 import Ground from "./Ground.js";
 import CactiController from "./CactiController.js";
-import BirdController from "./BirdController.js"; // Nowa klasa dla ptaków
 import Score from "./Score.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-// Konfiguracja gry
-const GAME_SPEED_START = 1; 
+const GAME_SPEED_START = 1; // 1.0
 const GAME_SPEED_INCREMENT = 0.00001;
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 200;
-
-// Konfiguracja gracza
-const PLAYER_WIDTH = 88 / 1.5; 
-const PLAYER_HEIGHT = 94 / 1.5; 
+const PLAYER_WIDTH = 88 / 1.5; //58
+const PLAYER_HEIGHT = 94 / 1.5; //62
 const MAX_JUMP_HEIGHT = GAME_HEIGHT;
 const MIN_JUMP_HEIGHT = 150;
-
-// Konfiguracja ziemi
 const GROUND_WIDTH = 2400;
 const GROUND_HEIGHT = 24;
 const GROUND_AND_CACTUS_SPEED = 0.5;
 
-// Konfiguracja kaktusów
 const CACTI_CONFIG = [
   { width: 48 / 1.5, height: 100 / 1.5, image: "images/cactus_1.png" },
   { width: 98 / 1.5, height: 100 / 1.5, image: "images/cactus_2.png" },
   { width: 68 / 1.5, height: 70 / 1.5, image: "images/cactus_3.png" },
 ];
 
-// Obiekty gry
+//Game Objects
 let player = null;
 let ground = null;
 let cactiController = null;
-let birdController = null; // Nowy kontroler dla ptaków
 let score = null;
 
 let scaleRatio = null;
 let previousTime = null;
 let gameSpeed = GAME_SPEED_START;
 let gameOver = false;
-let deathSoundPlayed = false; // Kontrola odtwarzania dźwięku
 let hasAddedEventListenersForRestart = false;
 let waitingToStart = true;
 
-const deathSound = new Audio('images/smutnasprawa.mp4'); // Ścieżka do dźwięku porażki
-
 function createSprites() {
-  scaleRatio = getScaleRatio();
+  const playerWidthInGame = PLAYER_WIDTH * scaleRatio;
+  const playerHeightInGame = PLAYER_HEIGHT * scaleRatio;
+  const minJumpHeightInGame = MIN_JUMP_HEIGHT * scaleRatio;
+  const maxJumpHeightInGame = MAX_JUMP_HEIGHT * scaleRatio;
 
-  player = new Player(ctx, PLAYER_WIDTH * scaleRatio, PLAYER_HEIGHT * scaleRatio, MIN_JUMP_HEIGHT * scaleRatio, MAX_JUMP_HEIGHT * scaleRatio, scaleRatio);
-  ground = new Ground(ctx, GROUND_WIDTH * scaleRatio, GROUND_HEIGHT * scaleRatio, GROUND_AND_CACTUS_SPEED, scaleRatio);
-  
-  const cactiImages = CACTI_CONFIG.map(cactus => ({
-    image: new Image().src = cactus.image,
-    width: cactus.width * scaleRatio,
-    height: cactus.height * scaleRatio
-  }));
-  
-  cactiController = new CactiController(ctx, cactiImages, scaleRatio, GROUND_AND_CACTUS_SPEED);
-  
-  // Przykład inicjalizacji BirdController, potrzebujesz zaimplementować logikę tej klasy
-  birdController = new BirdController(ctx, scaleRatio, GROUND_AND_CACTUS_SPEED);
-  
+  const groundWidthInGame = GROUND_WIDTH * scaleRatio;
+  const groundHeightInGame = GROUND_HEIGHT * scaleRatio;
+
+  player = new Player(
+    ctx,
+    playerWidthInGame,
+    playerHeightInGame,
+    minJumpHeightInGame,
+    maxJumpHeightInGame,
+    scaleRatio
+  );
+
+  ground = new Ground(
+    ctx,
+    groundWidthInGame,
+    groundHeightInGame,
+    GROUND_AND_CACTUS_SPEED,
+    scaleRatio
+  );
+
+  const cactiImages = CACTI_CONFIG.map((cactus) => {
+    const image = new Image();
+    image.src = cactus.image;
+    return {
+      image: image,
+      width: cactus.width * scaleRatio,
+      height: cactus.height * scaleRatio,
+    };
+  });
+
+  cactiController = new CactiController(
+    ctx,
+    cactiImages,
+    scaleRatio,
+    GROUND_AND_CACTUS_SPEED
+  );
+
   score = new Score(ctx, scaleRatio);
 }
 
@@ -77,17 +91,35 @@ function setScreen() {
   createSprites();
 }
 
-function reset() {
-  deathSoundPlayed = false;
-  gameOver = false;
-  waitingToStart = false;
-  ground.reset();
-  cactiController.reset();
-  birdController.reset(); // Resetuj również ptaki
-  score.reset();
-  gameSpeed = GAME_SPEED_START;
-  hasAddedEventListenersForRestart = false;
+setScreen();
+//Use setTimeout on Safari mobile rotation otherwise works fine on desktop
+window.addEventListener("resize", () => setTimeout(setScreen, 500));
+
+if (screen.orientation) {
+  screen.orientation.addEventListener("change", setScreen);
 }
+
+function getScaleRatio() {
+  const screenHeight = Math.min(
+    window.innerHeight,
+    document.documentElement.clientHeight
+  );
+
+  const screenWidth = Math.min(
+    window.innerWidth,
+    document.documentElement.clientWidth
+  );
+
+  //window is wider than the game width
+  if (screenWidth / screenHeight < GAME_WIDTH / GAME_HEIGHT) {
+    return screenWidth / GAME_WIDTH;
+  } else {
+    return screenHeight / GAME_HEIGHT;
+  }
+}
+
+// Definicja dźwięku porażki
+const deathSound = new Audio('images/smutnasprawa.mp4'); // Zalecane jest przeniesienie do folderu 'sounds'
 
 function showGameOver() {
   const fontSize = 70 * scaleRatio;
@@ -97,22 +129,29 @@ function showGameOver() {
   const y = canvas.height / 2;
   ctx.fillText("Koniec tego dobrego!", x, y);
 
-  if (!deathSoundPlayed) {
-    deathSound.play();
-    deathSoundPlayed = true;
-  }
-  
-  setupGameReset();
+  // Odtwórz dźwięk porażki
+  deathSound.play();
 }
 
 function setupGameReset() {
   if (!hasAddedEventListenersForRestart) {
     hasAddedEventListenersForRestart = true;
+
     setTimeout(() => {
       window.addEventListener("keyup", reset, { once: true });
       window.addEventListener("touchstart", reset, { once: true });
     }, 1000);
   }
+}
+
+function reset() {
+  hasAddedEventListenersForRestart = false;
+  gameOver = false;
+  waitingToStart = false;
+  ground.reset();
+  cactiController.reset();
+  score.reset();
+  gameSpeed = GAME_SPEED_START;
 }
 
 function showStartGameText() {
@@ -121,7 +160,16 @@ function showStartGameText() {
   ctx.fillStyle = "grey";
   const x = canvas.width / 14;
   const y = canvas.height / 2;
-  ctx.fillText("Naciśnij ekran bądź spację", x, y);
+  ctx.fillText("Naciśnij ekran bądz spacje", x, y);
+}
+
+function updateGameSpeed(frameTimeDelta) {
+  gameSpeed += frameTimeDelta * GAME_SPEED_INCREMENT;
+}
+
+function clearScreen() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function gameLoop(currentTime) {
@@ -136,52 +184,36 @@ function gameLoop(currentTime) {
   clearScreen();
 
   if (!gameOver && !waitingToStart) {
+    //Update game objects
     ground.update(gameSpeed, frameTimeDelta);
     cactiController.update(gameSpeed, frameTimeDelta);
-    birdController.update(gameSpeed, frameTimeDelta); // Aktualizuj ptaki
     player.update(gameSpeed, frameTimeDelta);
     score.update(frameTimeDelta);
     updateGameSpeed(frameTimeDelta);
-    
-    if (cactiController.collideWith(player) || birdController.collideWith(player)) {
-      gameOver = true;
-      score.setHighScore();
-    }
   }
 
+  if (!gameOver && cactiController.collideWith(player)) {
+    gameOver = true;
+    setupGameReset();
+    score.setHighScore();
+  }
+
+  //Draw game objects
   ground.draw();
   cactiController.draw();
-  birdController.draw(); // Rysuj ptaki
   player.draw();
   score.draw();
 
-  if (gameOver) showGameOver();
-  if (waitingToStart) showStartGameText();
+  if (gameOver) {
+    showGameOver();
+  }
+
+  if (waitingToStart) {
+    showStartGameText();
+  }
 
   requestAnimationFrame(gameLoop);
 }
-
-function clearScreen() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function getScaleRatio() {
-  const screenHeight = Math.min(window.innerHeight, document.documentElement.clientHeight);
-  const screenWidth = Math.min(window.innerWidth, document.documentElement.clientWidth);
-
-  return screenWidth / screenHeight < GAME_WIDTH / GAME_HEIGHT ? screenWidth / GAME_WIDTH : screenHeight / GAME_HEIGHT;
-}
-
-function updateGameSpeed(frameTimeDelta) {
-  gameSpeed += frameTimeDelta * GAME_SPEED_INCREMENT;
-}
-
-setScreen();
-window.addEventListener("resize", () => setTimeout(setScreen, 500));
-if (screen.orientation) screen.orientation.addEventListener("change", setScreen);
-window.addEventListener("keyup", (e) => { if(e.code === "Space" && waitingToStart) reset(); });
-window.addEventListener("touchstart", () => { if(waitingToStart) reset(); });
 
 requestAnimationFrame(gameLoop);
 
